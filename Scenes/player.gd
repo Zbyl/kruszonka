7bd88c2a8dafdf7af4eea247a00380783f921e57
Zbigniew Skowron <zbychs@gmail.com>
2024-01-26 17:50:35 +0100

    Player is looking around with right stick.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -10,11 +10,17 @@ const SPEED = 300.0
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		velocity = direction * SPEED
+	var move_direction = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+	var look_direction = Input.get_vector("rotate_left", "rotate_right", "rotate_up", "rotate_down")
+	look_direction = Vector2(-look_direction.y, look_direction.x)
+	#print("move_direction", move_direction)
+	if move_direction:
+		velocity = move_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	if look_direction:
+		look_at(global_position + look_direction)
 
 	move_and_slide()
