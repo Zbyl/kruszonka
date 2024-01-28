2118914fc76e11f2ba0f57744dec9c921d197fe0
Zbigniew Skowron <zbychs@gmail.com>
2024-01-28 13:01:08 +0100

    Added wizard.

--- Chef.gd
+++ Chef.gd
@@ -6,6 +6,7 @@ var enemy_container: Node2D
 const LOOK_AT_PLAYER_DISTANCE = 64 * 5
 const SPEAK_DISTANCE = 64 * 5
 var can_speak: bool = true
+@export var is_wizard: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,19 +22,33 @@ func _process(_delta):
 	var vec_to_player = player.global_position - global_position
 	if vec_to_player.length() < LOOK_AT_PLAYER_DISTANCE:
 		picture.look_at(player.global_position)
-	if vec_to_player.length() < SPEAK_DISTANCE:
-		if can_speak and Input.is_action_just_pressed("action"):
-			#Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
-			#get_tree().paused = true
-			GameData.game.pause_player_and_enemies(true)
-			can_speak = false
-			Dialogic.timeline_ended.connect(_dialog_ended)
-			Dialogic.start("hello")
+		
+	if not is_wizard:
+		if vec_to_player.length() < SPEAK_DISTANCE:
+			if can_speak and Input.is_action_just_pressed("action"):
+				GameData.game.pause_player_and_enemies(true)
+				can_speak = false
+				Dialogic.timeline_ended.connect(_dialog_ended)
+				Dialogic.start("hello")
 			
 
 func _dialog_ended():
 	Dialogic.timeline_ended.disconnect(_dialog_ended)
 	GameData.game.pause_player_and_enemies(false)
 	$Cooldown.start()
-	#get_tree().paused = false
 	GameData.hud.allow_croissant()
+
+
+func _wizard_dialog_ended():
+	Dialogic.timeline_ended.disconnect(_wizard_dialog_ended)
+	GameData.game.pause_player_and_enemies(false)
+	$Cooldown.start()
+
+
+func _on_area_2d_body_entered(body):
+	if is_wizard:
+		if can_speak:
+			GameData.game.pause_player_and_enemies(true)
+			can_speak = false
+			Dialogic.timeline_ended.connect(_wizard_dialog_ended)
+			Dialogic.start("wizard")
